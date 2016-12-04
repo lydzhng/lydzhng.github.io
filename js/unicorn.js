e4a113fd3315c3b7809eddf21e823ede7cd66c1f
lydzhng <lydzhng@gmail.com>
2016-12-03 19:01:50 -0500

    fart

--- js/unicorn.js
+++ js/unicorn.js
@@ -1,4 +1,6 @@
 function unicornClick() {
+	var snd = new Audio("sounds/fart.wav");
+	snd.play();
 	var poopImg = document.getElementById("poop")
 	poopImg.className = "start-poop";
 	setTimeout(function(){
