e14c52dc5fdc44cafc0d6cbca0773ee725620396
lydzhng <lydzhng@gmail.com>
2016-12-03 19:27:22 -0500

    counter addition

--- js/unicorn.js
+++ js/unicorn.js
@@ -1,8 +1,14 @@
+var unicorn = {}; 
+unicorn.counter = 0;
+var counterdiv = document.getElementById('counter')
+counterdiv.innerHTML = unicorn.counter
 function unicornClick() {
 	var snd = new Audio("sounds/fart.wav");
 	snd.play();
 	var poopImg = document.getElementById("poop")
 	poopImg.className = "start-poop";
+	unicorn.counter++
+	counterdiv.innerHTML = unicorn.counter
 	setTimeout(function(){
 		poopImg.className = "ground-poop";
 	},99)
