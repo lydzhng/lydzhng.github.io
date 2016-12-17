92b61a6ebc76146860932319478967bb782a631e
feychou <federica.recanatini@gmail.com>
2016-12-17 01:24:18 +0100

    Convert indentation to spaces

--- js/unicorn.js
+++ js/unicorn.js
@@ -3,17 +3,17 @@ unicorn.counter = 0;
 var counterdiv = document.getElementById('counter')
 counterdiv.innerHTML = unicorn.counter
 function unicornClick() {
-	var snd = new Audio("sounds/fart.wav");
-	snd.play();
-	var poopImg = document.getElementById("poop");
+  var snd = new Audio("sounds/fart.wav");
+  snd.play();
+  var poopImg = document.getElementById("poop");
   var pusheen = document.getElementById("pusheen");
-	poopImg.className = "start-poop";
+  poopImg.className = "start-poop";
   pusheen.className = "bounce";
-	unicorn.counter++
-	counterdiv.innerHTML = unicorn.counter
-	setTimeout(function(){
-		poopImg.className = "ground-poop";
-	},99);
+  unicorn.counter++
+  counterdiv.innerHTML = unicorn.counter
+  setTimeout(function(){
+    poopImg.className = "ground-poop";
+  },99);
   setTimeout(function() {
     pusheen.className = "";
   }, 500);
